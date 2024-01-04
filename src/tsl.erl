6e17f7e105fc773723ed97b1fb8db83d5f570976
Anatoly Rodionov <a.rodionov@samsung.com>
2024-01-04 16:38:55 -0500

    tsc improved

--- src/tsl.erl
+++ src/tsl.erl
@@ -1,27 +1,38 @@
 -module(tsl).
 
--export([t/4, tst/4]).
+-export([t/4, t/5, ts/3, ts/4, tst/4, tst/5]).
 
-tst(Mod, Fn, Args) ->
-    {Time, _} = timer:tc(Mod, Fn, Args),
+-define(DEFAULT_UNIT, microsecond).
+% second | millisecond | microsecond | nanosecond | native | perf_counter
+ts(Mod, Fn, Args) ->
+    ts(Mod, Fn, Args, ?DEFAULT_UNIT).
+
+ts(Mod, Fn, Args, Unit) ->
+    {Time, _} = timer:tc(Mod, Fn, Args, Unit),
     Time.
 
 tst(Mod, Fn, N, R) ->
+    tst(Mod, Fn, N, R, ?DEFAULT_UNIT).
+
+tst(Mod, Fn, N, R, Unit) ->
     Args = Mod:mk_args(Fn, N),
-    io:format("~p:~p, N=~p, R=~p, Time=~p(microseconds)~n", [
+    io:format("~p:~p, N=~p, R=~p, Time=~p(~p)~n", [
         Mod,
         Fn,
         N,
         R,
-        lists:sum([tst(Mod, Fn, Args) || _ <- lists:seq(1, R)]) / R
+        lists:sum([ts(Mod, Fn, Args, Unit) || _ <- lists:seq(1, R)]) / R,
+        Unit
     ]).
 
-t(Mod, Fn, Args, R) when is_list(Mod) ->
-    [t(M, Fn, Args, R) || M <- Mod],
+t(Mod, Fn, Args, R) -> t(Mod, Fn, Args, R, ?DEFAULT_UNIT).
+
+t(Mod, Fn, Args, R, Unit) when is_list(Mod) ->
+    [t(M, Fn, Args, R, Unit) || M <- Mod],
     ok;
-t(Mod, Fn, Args, R) when is_list(Fn) ->
-    [t(Mod, F, Args, R) || F <- Fn],
+t(Mod, Fn, Args, R, Unit) when is_list(Fn) ->
+    [t(Mod, F, Args, R, Unit) || F <- Fn],
     ok;
-t(Mod, Fn, Args, R) ->
-    spawn(fun() -> tst(Mod, Fn, Args, R) end),
+t(Mod, Fn, Args, R, Unit) ->
+    spawn(fun() -> tst(Mod, Fn, Args, R, Unit) end),
     ok.
