fde43193098e0e99bf9335374b52a8027138b533
Mikhail Khomenkov <mijkenator@gmail.com>
2024-02-26 10:32:50 -0500

    fix test funcs

--- src/aspike_tfuncs.erl
+++ src/aspike_tfuncs.erl
@@ -6,9 +6,9 @@
 ]).
 
 init(Password) ->
-    SeedIp = ?GET_ENV(aspike_seed_ip, "172.30.80.201"),
-    SeedPort = ?GET_ENV(aspike_seed_port, 3000),
-    User = ?GET_ENV(aspike_user, "gateway-service"),
+    SeedIp = "172.30.80.201",
+    SeedPort = 3000,
+    User = "gateway-service",
 
     application:set_env(aspike_port, host, SeedIp),
     application:set_env(aspike_port, port, SeedPort),
@@ -22,13 +22,20 @@ init(Password) ->
 
 test_insert(0, _) -> ok;
 test_insert(N, TTL) ->
-    Key = 
+    %Bins = [
+    %    {<<"column1">>, base64:encode(crypto:strong_rand_bytes(20))},
+    %    {<<"column2">>, base64:encode(crypto:strong_rand_bytes(10))},
+    %    {<<"timestamps">>, base64:encode(crypto:strong_rand_bytes(24))}
+    %],
+    %Key = base64:encode(crypto:strong_rand_bytes(30)),
+    Key = <<"test">>,
     Bins = [
-        {<<"column1">>, base64:encode(crypto:strong_rand_bytes(20))},
-        {<<"column2">>, base64:encode(crypto:strong_rand_bytes(10))},
-        {<<"timestamps">>, base64:encode(crypto:strong_rand_bytes(24))}
+        {<<"column1">>, <<"column1">>},
+        {<<"column2">>, <<"columb2">>},
+        {<<"timestamps">>, <<"00000000000000000001">>}
     ],
-    Key = base64:encode(crypto:strong_rand_bytes(30)),
-    aspike_nif:binary_put(<<"rtb-gateway">>, <<"mkh_perf_test">>, Key, Bins, TTL),
+
+    Ret = aspike_nif:binary_put(<<"rtb-gateway">>, <<"mkh_perf_test">>, Key, Bins, TTL), 
+    io:format("Ret: ~p ~n", [Ret]),
     timer:sleep(10),
     test_insert(N - 1, TTL).
